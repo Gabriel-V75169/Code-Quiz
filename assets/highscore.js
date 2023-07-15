ee3460472206edf89b48c48becf09ab9555b355d
Gabriel Valencia <jrjr75169@gmail.com>
2023-07-15 01:42:09 -0700

    a lot of code

--- assets/highscore.js
+++ assets/highscore.js
@@ -1,27 +1,28 @@
 
-var initial = document.getElementById('highscore');
-var timeleft = localStorage.getItem('timeleft');
-saveButton = document.getElementById('saveButton');
 
-saveButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    var highscore = {
-        initial: initial.value,
-        time: timeleft
-    };
+// var initial = document.getElementById('highscore');
+// var timeleft = localStorage.getItem('timeleft');
+// saveButton = document.getElementById('saveButton');
+
+// saveButton.addEventListener("click", function(event) {
+//     event.preventDefault();
+//     var highscore = {
+//         initial: initial.value,
+//         time: timeleft.valueOf
+//     };
     
-},);
+// },);
 
-function saveHighscores() {
-    localStorage.setItem("highscore", JSON.stringify(highscore));
-    renderMessage();
-}
-function renderMessage() {
-    var lastscore = JSON.parse(localStorage.getItem("highscore"));
-    if (lastscore !== null) {
-      document.querySelector(".scores").textContent = lastscore.initial + 
-      " received a/an " + timeLeft
-    }
-  }
+// function saveHighscores() {
+//     localStorage.setItem("highscore", JSON.stringify(highscore));
+//     renderMessage();
+// }
+// function renderMessage() {
+//     var lastscore = JSON.parse(localStorage.getItem("highscore"));
+//     if (lastscore !== null) {
+//       document.querySelector(".scores").textContent = lastscore.initial + 
+//       " received a/an " + timeLeft
+//     }
+//   }
 
-saveHighscores()
+// saveHighscores()
--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,79 @@
 var questions = document.getElementsByClassName("question");
+
 var timerEl = document.getElementById('timer');
 var correctAnswer = document.querySelector("#correct-answer")
-var timeLeft = 80; 
+var timeLeft = 120; 
 var initial = document.getElementById('highscore')
 
 function timer() {
   
     var timeInterval = setInterval(function () {
-      if (timeLeft > 1) {
+      if (timeLeft >= 0) {
         timerEl.textContent = "Time: "+timeLeft;
         timeLeft--;
       } else {
-        timerEl.textContent = 'Game Over';
+        window.location.href = 'endScreen.html';
         clearInterval(timeInterval);
       }
     }, 1000);  
 } 
 
+var container = document.querySelector(".container");
+
+window.addEventListener('click', ({ target }) => {
+    
+    if (target.matches(".answers-1")) { 
+      document.getElementById("Q1").style.display = "none";
+      
+      document.getElementById("Q2").style.display = "block";  
+    }
+  });
+
+  var container = document.querySelector(".container");
+
+window.addEventListener('click', ({ target }) => {
+    
+    if (target.matches('.answers-2')) { 
+      document.getElementById("Q2").style.display = "none";
+      
+      document.getElementById("Q3").style.display = "block";  
+    }
+  });
+
+  var container = document.querySelector(".container");
+
+window.addEventListener('click', ({ target }) => {
+    
+    if (target.matches('.answers-3')) { 
+      document.getElementById("Q3").style.display = "none";
+      
+      document.getElementById("Q4").style.display = "block";  
+    }
+  });
+
+  var container = document.querySelector(".container");
+
+window.addEventListener('click', ({ target }) => {
+    
+    if (target.matches('.answers-4')) { 
+      document.getElementById("Q4").style.display = "none";
+      
+      document.getElementById("Q5").style.display = "block";  
+    }
+  });
+  
+  window.addEventListener('click', ({ target }) => {
+    
+    if (target.matches('.answers-5')) { 
+      document.getElementById("Q5").style.display = "none";
+      
+      document.getElementById("Q5").style.display = "block";  
+
+      window.location.href = 'endScreen.html';
+    }
+  });
+      
+ 
 
 
 
@@ -26,7 +83,7 @@ timer();
 function savingTime(){
     localStorage.setItem('timeleft', JSON.stringify(timeLeft));
 };
-savingTime();
+savingTime()
 
 
 
