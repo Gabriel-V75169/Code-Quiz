27f7020811d387be7faa84357201ede060a3ec5f
Gabriel Valencia <jrjr75169@gmail.com>
2023-07-16 03:06:22 -0700

    finishing touches

--- assets/highscore.js
+++ assets/highscore.js
@@ -1,28 +1,36 @@
 
+var time = localStorage.getItem('timeleft');
+saveButton = document.getElementById('saveButton');
 
-// var initial = document.getElementById('highscore');
-// var timeleft = localStorage.getItem('timeleft');
-// saveButton = document.getElementById('saveButton');
 
-// saveButton.addEventListener("click", function(event) {
-//     event.preventDefault();
-//     var highscore = {
-//         initial: initial.value,
-//         time: timeleft.valueOf
-//     };
+localStorage.setItem("timeleft", JSON.stringify(time));
+
+function saveHighscores() { 
+    var inputInitial = document.querySelector("#high").value;
     
-// },);
+    
+    localStorage.setItem('initialSave', JSON.stringify(inputInitial));
+   
 
-// function saveHighscores() {
-//     localStorage.setItem("highscore", JSON.stringify(highscore));
-//     renderMessage();
-// }
-// function renderMessage() {
-//     var lastscore = JSON.parse(localStorage.getItem("highscore"));
-//     if (lastscore !== null) {
-//       document.querySelector(".scores").textContent = lastscore.initial + 
-//       " received a/an " + timeLeft
-//     }
-//   }
+    var highscore = {
+        initial: inputInitial,
+        time: time,
+    };
+    localStorage.setItem("highscore", JSON.stringify(highscore));
 
-// saveHighscores()
+    inputInitial.push(highscore)
+
+    localStorage.setItem("key", json.stringify(highscore));
+    
+}
+
+
+function renderMessage() {
+    var lastscore = JSON.parse(localStorage.getItem("highscore"));
+    if (lastscore !== null) {
+      document.querySelector("#scores").textContent = lastscore.initial + 
+      " score is, " + time
+    }
+  }
+
+    
--- assets/script.js
+++ assets/script.js
@@ -5,34 +5,50 @@ var correctAnswer = document.querySelector("#correct-answer")
 var timeLeft = 120; 
 var initial = document.getElementById('highscore')
 
+
+function savingTime(){
+    localStorage.setItem('timeleft', JSON.stringify(timeLeft));
+};
 function timer() {
   
     var timeInterval = setInterval(function () {
-      if (timeLeft >= 0) {
+      if (timeLeft > 0) {
         timerEl.textContent = "Time: "+timeLeft;
         timeLeft--;
       } else {
         window.location.href = 'endScreen.html';
         clearInterval(timeInterval);
+        savingTime()
       }
     }, 1000);  
 } 
 
+
+
+  
+  function myFunction() {
+    timeLeft = timeLeft - 10;
+  }
+
+
 var container = document.querySelector(".container");
 
 window.addEventListener('click', ({ target }) => {
-    
-    if (target.matches(".answers-1")) { 
+  
+
+    if (target.matches(".answers-1")) {  
+      
       document.getElementById("Q1").style.display = "none";
       
       document.getElementById("Q2").style.display = "block";  
     }
   });
-
+  
   var container = document.querySelector(".container");
 
 window.addEventListener('click', ({ target }) => {
-    
+  
+
     if (target.matches('.answers-2')) { 
       document.getElementById("Q2").style.display = "none";
       
@@ -43,7 +59,8 @@ window.addEventListener('click', ({ target }) => {
   var container = document.querySelector(".container");
 
 window.addEventListener('click', ({ target }) => {
-    
+  
+
     if (target.matches('.answers-3')) { 
       document.getElementById("Q3").style.display = "none";
       
@@ -56,6 +73,8 @@ window.addEventListener('click', ({ target }) => {
 window.addEventListener('click', ({ target }) => {
     
     if (target.matches('.answers-4')) { 
+      
+
       document.getElementById("Q4").style.display = "none";
       
       document.getElementById("Q5").style.display = "block";  
@@ -64,10 +83,13 @@ window.addEventListener('click', ({ target }) => {
   
   window.addEventListener('click', ({ target }) => {
     
+
     if (target.matches('.answers-5')) { 
       document.getElementById("Q5").style.display = "none";
       
       document.getElementById("Q5").style.display = "block";  
+      
+      savingTime()
 
       window.location.href = 'endScreen.html';
     }
@@ -75,15 +97,12 @@ window.addEventListener('click', ({ target }) => {
       
  
 
-
-
+  
 
    
 timer();
-function savingTime(){
-    localStorage.setItem('timeleft', JSON.stringify(timeLeft));
-};
-savingTime()
+
+
 
 
 
